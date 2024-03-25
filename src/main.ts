19dfdda8ba4fe438b1e637a0b518e94a125fa2ae
meenahiwatashi <meenahiwatashi@gmail.com>
2024-03-25 09:10:41 -0300

    start: 🎉

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/aura-light-noir/theme.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(PrimeVue)
+app.mount('#app')
