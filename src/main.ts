5c2b0b747518aa6b57c8eb10ad9a8906936b9f37
meenahiwatashi <meenahiwatashi@gmail.com>
2024-03-26 09:47:27 -0300

    chore: 🔧 importando arquivos, e adicionando o efeito ripple nas config do priemvue

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import './assets/base.css'
+import 'primeicons/primeicons.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,5 +10,7 @@ import 'primevue/resources/themes/aura-light-noir/theme.css'
 const app = createApp(App)
 
 app.use(router)
-app.use(PrimeVue)
+app.use(PrimeVue, {
+  ripple: true
+})
 app.mount('#app')
