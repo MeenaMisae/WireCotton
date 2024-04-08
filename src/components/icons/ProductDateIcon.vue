6aa8a0a1183f3a7f925ba16e27bc6f3948cb5a02
meenahiwatashi <meenahiwatashi@gmail.com>
2024-04-08 08:42:05 -0300

    feat: 🍱 icone de calendario

---
+++ src/components/icons/ProductDateIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M15.0861 2H4.00468C3.13039 2 2.42163 2.67157 2.42163 3.5V14C2.42163 14.8284 3.13039 15.5 4.00468 15.5H15.0861C15.9604 15.5 16.6691 14.8284 16.6691 14V3.5C16.6691 2.67157 15.9604 2 15.0861 2Z"
+        stroke="#292929"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2.42163 6.5H16.6691"
+        stroke="#292929"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12.047 1.5V4.5"
+        stroke="#292929"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M6.047 1.5V4.5"
+        stroke="#292929"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </div>
+</template>
