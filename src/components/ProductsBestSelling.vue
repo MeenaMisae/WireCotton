3616864ec4bcfd8d747e512aa6ce0cbebc87b0f3
meenahiwatashi <meenahiwatashi@gmail.com>
2024-04-08 10:40:08 -0300

    feat: ✨ componente dos produtos mais vendidos

---
+++ src/components/ProductsBestSelling.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="ml-8">
+    <h3 class="text-2xl font-semibold">Produtos mais vendidos</h3>
+    <div class="shadow-md py-7 px-5 w-[23.3rem] mt-4 space-y-7">
+      <div class="flex">
+        <div class="h-40">
+          <img
+            src="/src/assets/images/t-shirt-kiikii.avif"
+            alt="camisa kiikii"
+            class="h-full shadow rounded-md"
+          />
+        </div>
+        <div class="ml-8">
+          <h4 class="font-semibold text-lg">Camisa KiiKii</h4>
+          <div class="flex space-x-3 mt-2">
+            <div class="p-1 h-6 w-6 shadow-md rounded">
+              <div class="bg-[#CCBFB6] h-full w-full rounded-sm"></div>
+            </div>
+            <div class="p-1 h-6 w-6 shadow-md rounded">
+              <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
+                >M</span
+              >
+            </div>
+            <div class="p-2 h-6 shadow-md rounded">
+              <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
+                >R$ 35,90</span
+              >
+            </div>
+          </div>
+          <div class="mt-7">
+            <p class="mb-1">Último mês: 35</p>
+            <p>Total vendido: 123</p>
+          </div>
+        </div>
+      </div>
+      <div class="flex">
+        <div class="h-40">
+          <img
+            src="/src/assets/images/t-shirt-kiikii.avif"
+            alt="camisa kiikii"
+            class="h-full shadow rounded-md"
+          />
+        </div>
+        <div class="ml-8">
+          <h4 class="font-semibold text-lg">Camisa KiiKii</h4>
+          <div class="flex space-x-3 mt-2">
+            <div class="p-1 h-6 w-6 shadow-md rounded">
+              <div class="bg-[#CCBFB6] h-full w-full rounded-sm"></div>
+            </div>
+            <div class="p-1 h-6 w-6 shadow-md rounded">
+              <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
+                >M</span
+              >
+            </div>
+            <div class="p-2 h-6 shadow-md rounded">
+              <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
+                >R$ 35,90</span
+              >
+            </div>
+          </div>
+          <div class="mt-7">
+            <p class="mb-1">Último mês: 35</p>
+            <p>Total vendido: 123</p>
+          </div>
+        </div>
+      </div>
+      <div class="flex">
+        <div class="h-40">
+          <img
+            src="/src/assets/images/t-shirt-kiikii.avif"
+            alt="camisa kiikii"
+            class="h-full shadow rounded-md"
+          />
+        </div>
+        <div class="ml-8">
+          <h4 class="font-semibold text-lg">Camisa KiiKii</h4>
+          <div class="flex space-x-3 mt-2">
+            <div class="p-1 h-6 w-6 shadow-md rounded">
+              <div class="bg-[#CCBFB6] h-full w-full rounded-sm"></div>
+            </div>
+            <div class="p-1 h-6 w-6 shadow-md rounded">
+              <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
+                >M</span
+              >
+            </div>
+            <div class="p-2 h-6 shadow-md rounded">
+              <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
+                >R$ 35,90</span
+              >
+            </div>
+          </div>
+          <div class="mt-7">
+            <p class="mb-1">Último mês: 35</p>
+            <p>Total vendido: 123</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
