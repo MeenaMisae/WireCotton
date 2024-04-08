1a30feeb458a2e9c2caba6c34e80dbbc4e77b01e
meenahiwatashi <meenahiwatashi@gmail.com>
2024-04-08 08:39:51 -0300

    feat: 🍱 icone de seta direita

---
+++ src/components/icons/ArrowRightIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M2.32983 9.83337H17.7465"
+        stroke="black"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.9132 4L17.7465 9.83333L11.9132 15.6667"
+        stroke="black"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </div>
+</template>
