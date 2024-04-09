1ff233c735014e6b332052767add1662a0b8f891
meenahiwatashi <meenahiwatashi@gmail.com>
2024-04-09 10:57:14 -0300

    style: 💄 ajustando borda dos produtos mais vendidos

--- src/components/ProductsBestSelling.vue
+++ src/components/ProductsBestSelling.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ml-8">
     <h3 class="text-2xl font-semibold">Produtos mais vendidos</h3>
-    <div class="shadow-md py-7 px-5 w-[23.3rem] mt-4 space-y-7">
+    <div class="shadow-md border py-7 px-5 w-[23.3rem] mt-4 space-y-7">
       <div class="flex">
         <div class="h-40">
           <img
@@ -13,15 +13,15 @@
         <div class="ml-8">
           <h4 class="font-semibold text-lg">Camisa KiiKii</h4>
           <div class="flex space-x-3 mt-2">
-            <div class="p-1 h-6 w-6 shadow-md rounded">
+            <div class="p-1 h-6 w-6 shadow-md rounded border">
               <div class="bg-[#CCBFB6] h-full w-full rounded-sm"></div>
             </div>
-            <div class="p-1 h-6 w-6 shadow-md rounded">
+            <div class="p-1 h-6 w-6 shadow-md rounded border">
               <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
                 >M</span
               >
             </div>
-            <div class="p-2 h-6 shadow-md rounded">
+            <div class="p-2 h-6 shadow-md rounded border">
               <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
                 >R$ 35,90</span
               >
