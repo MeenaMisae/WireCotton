d780069bb4c2285db3e929260148a3ed6b91a663
meenahiwatashi <meenahiwatashi@gmail.com>
2024-04-14 10:34:54 -0300

    feat: 📱 melhoras na responsividade do card de produtos mais vendidos

--- src/components/ProductsBestSelling.vue
+++ src/components/ProductsBestSelling.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="ml-8">
+  <div class="lg:ml-8 mb-4 mt-8 lg:mb-0 lg:mt-0">
     <h3 class="text-2xl font-semibold">Produtos mais vendidos</h3>
-    <div class="shadow-md border py-7 px-5 w-[23.3rem] mt-4 space-y-7">
+    <div class="shadow-md border lg:py-7 lg:px-5 p-4 lg:w-[23.3rem] mt-4 space-y-7">
       <div class="flex">
         <div class="h-40">
           <img
@@ -11,24 +11,26 @@
           />
         </div>
         <div class="ml-8">
-          <h4 class="font-semibold text-lg">Camisa KiiKii</h4>
-          <div class="flex space-x-3 mt-2">
-            <div class="p-1 h-6 w-6 shadow-md rounded border">
-              <div class="bg-[#CCBFB6] h-full w-full rounded-sm"></div>
+          <h4 class="font-semibold lg:text-lg">Camisa KiiKii</h4>
+          <div class="flex mt-2 flex-col lg:flex-row lg:items-center">
+            <div class="flex gap-3">
+              <div class="p-1 h-6 w-6 shadow-md rounded border">
+                <div class="bg-[#CCBFB6] h-full w-full rounded-sm"></div>
+              </div>
+              <div class="p-1 h-6 w-6 shadow-md rounded border">
+                <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
+                  >M</span
+                >
+              </div>
             </div>
-            <div class="p-1 h-6 w-6 shadow-md rounded border">
-              <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
-                >M</span
-              >
-            </div>
-            <div class="p-2 h-6 shadow-md rounded border">
-              <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
-                >R$ 35,90</span
-              >
+            <div class="p-2 h-6 w-[5.4rem] lg:w-full shadow-md rounded border mt-2 lg:mt-0 lg:ml-3">
+              <span class="h-full w-full rounded-sm flex items-center justify-center text-base">
+                R$ 35,90
+              </span>
             </div>
           </div>
           <div class="mt-7">
-            <p class="mb-1">Último mês: 35</p>
+            <p class="lg:mb-1">Último mês: 35</p>
             <p>Total vendido: 123</p>
           </div>
         </div>
@@ -42,24 +44,26 @@
           />
         </div>
         <div class="ml-8">
-          <h4 class="font-semibold text-lg">Camisa KiiKii</h4>
-          <div class="flex space-x-3 mt-2">
-            <div class="p-1 h-6 w-6 shadow-md rounded">
-              <div class="bg-[#CCBFB6] h-full w-full rounded-sm"></div>
+          <h4 class="font-semibold lg:text-lg">Camisa KiiKii</h4>
+          <div class="flex mt-2 flex-col lg:flex-row lg:items-center">
+            <div class="flex gap-3">
+              <div class="p-1 h-6 w-6 shadow-md rounded border">
+                <div class="bg-[#CCBFB6] h-full w-full rounded-sm"></div>
+              </div>
+              <div class="p-1 h-6 w-6 shadow-md rounded border">
+                <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
+                  >M</span
+                >
+              </div>
             </div>
-            <div class="p-1 h-6 w-6 shadow-md rounded">
-              <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
-                >M</span
-              >
-            </div>
-            <div class="p-2 h-6 shadow-md rounded">
-              <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
-                >R$ 35,90</span
-              >
+            <div class="p-2 h-6 w-[5.4rem] lg:w-full shadow-md rounded border mt-2 lg:mt-0 lg:ml-3">
+              <span class="h-full w-full rounded-sm flex items-center justify-center text-base">
+                R$ 35,90
+              </span>
             </div>
           </div>
           <div class="mt-7">
-            <p class="mb-1">Último mês: 35</p>
+            <p class="lg:mb-1">Último mês: 35</p>
             <p>Total vendido: 123</p>
           </div>
         </div>
@@ -73,24 +77,26 @@
           />
         </div>
         <div class="ml-8">
-          <h4 class="font-semibold text-lg">Camisa KiiKii</h4>
-          <div class="flex space-x-3 mt-2">
-            <div class="p-1 h-6 w-6 shadow-md rounded">
-              <div class="bg-[#CCBFB6] h-full w-full rounded-sm"></div>
+          <h4 class="font-semibold lg:text-lg">Camisa KiiKii</h4>
+          <div class="flex mt-2 flex-col lg:flex-row lg:items-center">
+            <div class="flex gap-3">
+              <div class="p-1 h-6 w-6 shadow-md rounded border">
+                <div class="bg-[#CCBFB6] h-full w-full rounded-sm"></div>
+              </div>
+              <div class="p-1 h-6 w-6 shadow-md rounded border">
+                <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
+                  >M</span
+                >
+              </div>
             </div>
-            <div class="p-1 h-6 w-6 shadow-md rounded">
-              <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
-                >M</span
-              >
-            </div>
-            <div class="p-2 h-6 shadow-md rounded">
-              <span class="h-full w-full rounded-sm flex items-center justify-center text-base"
-                >R$ 35,90</span
-              >
+            <div class="p-2 h-6 w-[5.4rem] lg:w-full shadow-md rounded border mt-2 lg:mt-0 lg:ml-3">
+              <span class="h-full w-full rounded-sm flex items-center justify-center text-base">
+                R$ 35,90
+              </span>
             </div>
           </div>
           <div class="mt-7">
-            <p class="mb-1">Último mês: 35</p>
+            <p class="lg:mb-1">Último mês: 35</p>
             <p>Total vendido: 123</p>
           </div>
         </div>
