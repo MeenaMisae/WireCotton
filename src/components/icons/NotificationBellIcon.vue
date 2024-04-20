d6a56797bedc069363de021fbbe61c5558b9c659
meenahiwatashi <meenahiwatashi@gmail.com>
2024-04-20 13:52:11 -0300

    wip: 🚧 props do icone de notificação

--- src/components/icons/NotificationBellIcon.vue
+++ src/components/icons/NotificationBellIcon.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      :width="width"
+      :height="height"
+      viewBox="0 0 25 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path
         d="M18.9883 8C18.9883 6.4087 18.3561 4.88258 17.2309 3.75736C16.1057 2.63214 14.5796 2 12.9883 2C11.397 2 9.87086 2.63214 8.74564 3.75736C7.62042 4.88258 6.98828 6.4087 6.98828 8C6.98828 15 3.98828 17 3.98828 17H21.9883C21.9883 17 18.9883 15 18.9883 8Z"
         stroke="black"
@@ -18,3 +24,9 @@
     </svg>
   </div>
 </template>
+<script setup>
+defineProps({
+  width: Number,
+  height: Number
+})
+</script>
