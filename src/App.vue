0e0fc86b72017b02906636921c2a5152b1bae235
meenahiwatashi <meenahiwatashi@gmail.com>
2024-04-24 07:27:35 -0300

    feat: ✨ transição suave ao mudar de pagina

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,13 @@
       <SideMenu @visible-side-menu="hideOverflow" />
       <div class="lg:pr-10 lg:pl-5 lg:ml-8 w-[100%] px-6">
         <NavBar />
-        <RouterView />
+        <router-view v-slot="{ Component, route }">
+          <Transition name="fade" mode="out-in">
+            <div :key="route.path">
+              <component :is="Component"></component>
+            </div>
+          </Transition>
+        </router-view>
       </div>
     </div>
   </div>
@@ -20,3 +26,15 @@ function hideOverflow(event: boolean) {
   hide.value = event
 }
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
