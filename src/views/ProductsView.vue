65fdc17ba4530e2d2d0aeaade16821c5da610b0b
meenahiwatashi <meenahiwatashi@gmail.com>
2024-05-06 21:29:31 -0300

    wip: 🚧 link provisorio para novo produto

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div class="flex justify-end">
+    <RouterLink to="/products/create">
+      <button class="flex items-center shadow rounded h-14 w-16 justify-center">
+        <ProductPackageBlack />
+        <ArrowRightIcon />
+      </button>
+    </RouterLink>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import ProductPackageBlack from '@/components/icons/ProductPackageBlack.vue'
+import ArrowRightIcon from '@/components/icons/ArrowRightIcon.vue'
+</script>
